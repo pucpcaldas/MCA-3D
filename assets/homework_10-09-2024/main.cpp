64cddf7c7dabba3949f4a4ffd3ad2f4b3b3befe9
Davi Campos <davicampos.dev@gmail.com>
2024-09-10 21:34:53 -0300

    rename window

--- assets/homework_10-09-2024/main.cpp
+++ assets/homework_10-09-2024/main.cpp
@@ -65,7 +65,7 @@ int main(int argc, char** argv) {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(500, 500);
-    glutCreateWindow("Desenhando DAVI com Linhas");
+    glutCreateWindow("Name Render");
 
     init();
     glutDisplayFunc(render);
